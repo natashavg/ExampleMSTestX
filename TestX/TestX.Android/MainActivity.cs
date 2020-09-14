f0bb9634101339eb1c57f948338a9149967814f8
Natalya Gruntkovskiy <natashavg@yahoo.com>
2020-09-14 14:25:11 -0700

    Remove unnecessary MainActivity code in MSTest project

--- TestX/TestX.Android/MainActivity.cs
+++ TestX/TestX.Android/MainActivity.cs
@@ -14,12 +14,12 @@ namespace TestX.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+            //TabLayoutResource = Resource.Layout.Tabbar;
+            //ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new Xamarin.Forms.Application());
+            //global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            //LoadApplication(new Xamarin.Forms.Application());
         }
     }
 }
